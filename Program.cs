de32919c924e0090bdeb94df640d76abda449808
armaneeuu <armaninteresado@hotmail.com>
2024-11-18 15:21:02 -0500

    p

--- Program.cs
+++ Program.cs
@@ -5,11 +5,21 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura el puerto dinámicamente con la variable de entorno PORT proporcionada por Render
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    app.Urls.Add($"http://0.0.0.0:{port}"); // Escuchar en el puerto proporcionado por Render
+}
+else
+{
+    app.Urls.Add("http://0.0.0.0:5000"); // Puerto por defecto en caso de que no se encuentre la variable
+}
+
+// Configura el pipeline de solicitudes HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
