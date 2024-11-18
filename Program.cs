25dc2e3556363e5dff8d52a5ad7c0982fa10c571
armaneeuu <armaninteresado@hotmail.com>
2024-11-18 15:19:12 -0500

    p

--- Program.cs
+++ Program.cs
@@ -5,17 +5,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configurar el puerto para que sea dinámico
-var port = Environment.GetEnvironmentVariable("PORT");
-if (!string.IsNullOrEmpty(port))
-{
-    app.Urls.Add($"http://0.0.0.0:{port}"); // Usar el puerto proporcionado por Render
-}
-else
-{
-    app.Urls.Add("http://0.0.0.0:5000"); // Usar puerto 5000 por defecto si no se encuentra la variable PORT
-}
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
